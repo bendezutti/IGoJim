ddaeb87fcf54d74ca130021f939fc76026f74169
bendezutti <benjamindezutti@gmail.com>
2022-08-21 03:00:53 -0400

    commit

--- src/Components/ModalForm.js
+++ src/Components/ModalForm.js
@@ -1,25 +1,22 @@
 import { Form } from "react-bootstrap";
-import Dropdown from 'react-bootstrap'; 
 
-function handlerSubmission(e) { 
-alert(e.target.weight.value)
-}
+function weightSubmission(e) { 
+(e.target.weight.value)
+} 
 
 function ModalForm({closeModal}) {
   return (
     <div className = "modalBackGround"> 
         <div className = "modalContainer" >
-        <Form onSubmit = {handlerSubmission}>  
+        <Form onSubmit = {weightSubmission}>  
         <button className = "x" onClick = {()=> closeModal(false)}></button>
             <Form.Label>Enter Weight: </Form.Label>
-            <Form.Control
-              type = "text"  
-              name = "weight"
-             />
-             <button className= "Submit" type = "submit">Submit </button>
+            <Form.Control type = "number" name = "weight" />
+            <button className= "submitWeight" type = "submit">Submit </button>
          </Form> 
         </div>
     </div>
   )
-}
+} 
+
 export default ModalForm
