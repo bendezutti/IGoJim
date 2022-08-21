8f3ea11aec82aa012fe235ebc1f84b7bf2d6ac40
bendezutti <benjamindezutti@gmail.com>
2022-08-21 01:50:49 -0400

    Commit

--- rc/backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const http =  require("http");
-const socketIO = require("socket.io");
-
-var bodyParser = require("socket.io");
-const cors = require("cors");
-const port = process.env.PORT || 4001;
-const index = require(".")
-
